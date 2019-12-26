bb42fe766dae152c22ffb4fa773681eaf33c71b9
codecademy <ccuser@codecademy.com>
2019-12-26 13:05:43 +0000

    Added function to generate message for traveler and present attractions they might be interested in.

--- script.py
+++ script.py
@@ -36,7 +36,7 @@ def add_attraction(destination, attraction):
 add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
-add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
 add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
 add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
 add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
@@ -56,10 +56,32 @@ def find_attractions(destination, interests):
     for interest in interests:
       if interest in attraction_tags:
         attractions_with_interest.append(possible_attraction[0])
+        break
   return attractions_with_interest
 
 la_arts = find_attractions("Los Angeles, USA", ['art'])
-print(la_arts)
+#print(la_arts)
+#print(find_attractions(test_traveler[1], test_traveler[2]))
 
+def get_attractions_for_traveler(traveler):
+  traveler_destination = traveler[1]
+  traveler_interests = traveler[2]
+  traveler_attractions = find_attractions(traveler_destination, traveler_interests)
   
+  interest_string = "Hi "
+  interest_string += traveler[0]
+  interest_string += ", we think you'll like the places around " + traveler_destination + ":"
+  for attraction in traveler_attractions[:-1]:
+    interest_string += " " + attraction + ","
+  interest_string += " " + traveler_attractions[-1] + "."
+  
+  return interest_string
+
+
+smills_france = get_attractions_for_traveler(['Dereck Smill', 'Paris, France', ['monument']]) 
+print(smills_france)
+
+print(get_attractions_for_traveler(test_traveler))
+
+
   
